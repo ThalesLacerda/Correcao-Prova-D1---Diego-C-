a2c6f15e8abd8b4242548fa9ec8cc88925cc654d
Thales Lacerda <42879790+ThalesLacerda@users.noreply.github.com>
2018-10-16 20:28:22 -0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/IMC.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+        
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+
+        }
+
+        public IMC(double peso, double altura)
+        {
+
+            this.peso = peso;
+            this.altura = altura;
+
+        }
+
+        // Questão 3
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+
+
+        }
+
+
+        public double Classificar(double imc)
+        {
+            return 0.0;
+
+        }
+
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        // Questão 1
+         static int Maior(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+
+
+        static int Maior2(int a, int b)
+        {
+            return (a > b) ? a : b;
+
+        }
+
+
+        // Questão 2
+        static bool Par(int numero)
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return true;
+            else
+                return false;
+
+        }
+
+        static string Par2(int numero)
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return "Par";
+            else
+                return "Ímpar";
+        }
+
+
+
+
+            /*/ correção prova 
+
+               1) Maior (a,b);
+
+                  Se (a>b)
+                   retorna a;
+                  Senão
+                   retorna b;         
+           Tradução
+
+           static int Maior (int a, int b) {
+                   if (a > b)
+                       return a,
+                   else
+                       return b;
+                       }    /*/
+
+            static void Main(string[] args)
+        {
+
+        }
+    }
+}
